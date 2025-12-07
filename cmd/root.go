ecb6d9c9e62b3999bd8d6da684cad5810d196712
Jonas Peterburs <j.peterburs@protonmail.com>
2025-12-07 11:52:28 +0100

    feat: add viper for easy user config

--- cmd/root.go
+++ cmd/root.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var rootCmd = &cobra.Command{
@@ -21,4 +22,26 @@ func Execute() {
 }
 
 func init() {
+	cobra.OnInitialize(initConfig)
+}
+
+func initConfig() {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	viper.AddConfigPath(home)
+	viper.AddConfigPath(".")
+	viper.SetConfigType("yaml")
+	viper.SetConfigName(".ts3-cli")
+
+	viper.SetEnvPrefix("TS3")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		rootCmd.PrintErrln(err)
+		os.Exit(1)
+	}
 }
